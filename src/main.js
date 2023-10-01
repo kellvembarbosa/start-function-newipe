bcb77661587cdafed347cfed381a1a7a6bc4032c
Kellvem Barbosa <kellvem222@gmail.com>
2023-10-01 13:24:50 +0100

    test function

--- src/main.js
+++ src/main.js
@@ -5,10 +5,10 @@ import { Client } from 'node-appwrite';
 export default async ({ req, res, log, error }) => {
   // Why not try the Appwrite SDK?
   //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+  const client = new Client()
+     .setEndpoint('https://cloud.appwrite.io/v1')
+     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+     .setKey(process.env.APPWRITE_API_KEY);
 
   // You can log messages to the console
   log('Hello, Logs!');
@@ -17,17 +17,25 @@ export default async ({ req, res, log, error }) => {
   error('Hello, Errors!');
 
   // The `req` object contains the request data
+  if (req.method === 'POST' && req.path == '/webhook') {
+    // Send a response with the res object helpers
+    // `res.send()` dispatches a string back to the client
+    return res.json('Hello, World!');
+  }
+
+  // The `req` object contains the request data
   if (req.method === 'GET') {
     // Send a response with the res object helpers
     // `res.send()` dispatches a string back to the client
-    return res.send('Hello, World!');
+    return res.json({
+      ok: true,
+      message: 'ready to receive webhooks!'
+    });
   }
 
   // `res.json()` is a handy helper for sending JSON
   return res.json({
-    motto: 'Build Fast. Scale Big. All in One Place.',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
+    ok: false,
+    message: 'This is not a valid webhook endpoint'
   });
 };
